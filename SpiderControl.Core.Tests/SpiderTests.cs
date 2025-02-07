2e174898f7f89f7c4472e5698f8c945b21a47689
war <crypto22h@gmail.com>
2025-02-07 20:00:40 +0000

    TDD - write a failing test

---
+++ SpiderControl.Core.Tests/SpiderTests.cs
@@ -0,0 +1,15 @@
+﻿using SpiderControl.Core.Models;
+
+namespace SpiderControl.Core.Tests;
+
+public class SpiderTests
+{
+    [Fact]
+    public void Position_AfterInitialization_ReturnsStartingPosition()
+    {
+        var spider = new SpiderModel();
+
+        Assert.Equal(2, spider.X);
+        Assert.Equal(4, spider.Y);
+    }
+}
