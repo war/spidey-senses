44fc3f573b2c69f23ef67778ee727f653ccad701
war <crypto22h@gmail.com>
2025-02-08 05:48:42 +0000

    implement RotateLeft in spider service

--- SpiderControl.Core/Services/SpiderService.cs
+++ SpiderControl.Core/Services/SpiderService.cs
@@ -21,7 +21,23 @@ public class SpiderService : ISpiderService
 
     public void RotateLeft(SpiderModel model)
     {
-
+        switch (model.Orientation)
+        {
+            case Orientation.Up:
+                model.Orientation = Orientation.Left; 
+                break;
+            case Orientation.Left:
+                model.Orientation = Orientation.Down;
+                break;
+            case Orientation.Down:
+                model.Orientation = Orientation.Right;
+                break;
+            case Orientation.Right:
+                model.Orientation = Orientation.Up;
+                break;
+            default:
+                throw new ArgumentException("Invalid orientation");
+        }
     }
 
     public void RotateRight(SpiderModel model)
