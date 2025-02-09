72c3a692dd6ad79f88b7a7d220e01a292b338b60
war <crypto22h@gmail.com>
2025-02-09 18:37:58 +0000

    install tailwindcss

--- spider-control-web/src/app/app.component.spec.ts
+++ spider-control-web/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'SpiderControl.Web' title`, () => {
+  it(`should have the 'spider-control-web' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SpiderControl.Web');
+    expect(app.title).toEqual('spider-control-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SpiderControl.Web');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, spider-control-web');
   });
 });
