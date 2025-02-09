2425b4c09169f7cadccd5ec53d0a69e1daf87400
war <crypto22h@gmail.com>
2025-02-09 17:07:43 +0000

    implementation of the main entrypoint for the console project

---
+++ SpiderControl.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SpiderControl.Application.Interfaces;
+using SpiderControl.Application.Services;
+using SpiderControl.Core.Factories;
+using SpiderControl.Core.Interfaces;
+using SpiderControl.Core.Services;
+
+namespace SpiderControl.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSpiderControlServices(this IServiceCollection services)
+    {
+        services.AddSingleton<ISpiderService, SpiderService>();
+        services.AddSingleton<ICommandFactory, CommandFactory>();
+        services.AddSingleton<IInputParser, InputParser>();
+        services.AddSingleton<ISpiderApplicationService, SpiderApplicationService>();
+
+        return services;
+    }
+}
--- SpiderControl.Console/Program.cs
+++ SpiderControl.Console/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using SpiderControl.Application;
+using SpiderControl.Application.Interfaces;
+using SpiderControl.Console.IO;
+
+namespace SpiderControl.Console;
+
+public class Progam
+{
+    public static void Main(String[] args)
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var spiderApplicationService = serviceProvider.GetRequiredService<ISpiderApplicationService>();
+
+        var inputReader = new ConsoleInputReader();
+
+        var inputs = inputReader.ReadInputs();
+        var result = spiderApplicationService.ProcessSpiderCommands(
+            inputs.WallDimensions, 
+            inputs.SpiderPosition, 
+            inputs.Commands
+        );
+
+        System.Console.WriteLine($"Final Output: {result}.");
+        System.Console.WriteLine("Press any key to continue...");
+        System.Console.ReadLine();
+    }
+
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSpiderControlServices();
+    }
+}
+
