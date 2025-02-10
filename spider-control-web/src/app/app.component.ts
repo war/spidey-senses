e9dff7d7dc3ab243cae8173c1b9e0a6075330451
war <crypto22h@gmail.com>
2025-02-10 05:52:40 +0000

    add components to main component

--- spider-control-web/src/app/app.component.ts
+++ spider-control-web/src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SpiderComponent } from './components/spider/spider.component';
+import { ControlsComponent } from "./components/controls/controls.component";
+import { WallComponent } from "./components/wall/wall.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [SpiderComponent, ControlsComponent, WallComponent],
+  template: `
+    <nav class="bg-gray-800">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <h1 class="text-white">Spider Control</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <main>
+      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <app-spider></app-spider>
+        <app-wall></app-wall>
+        <app-controls></app-controls>
+      </div>
+    </main>
+  `
 })
+
 export class AppComponent {
   title = 'spider-control-web';
 }
