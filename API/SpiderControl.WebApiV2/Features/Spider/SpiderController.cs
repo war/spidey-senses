4d744b98af7cf4ffeea62b32cb22f146db4e5965
war <crypto22h@gmail.com>
2025-02-23 16:41:31 +0000

    implement correct status code for versions

---
+++ API/SpiderControl.WebApiV2/Features/Spider/SpiderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpiderControl.Api.Shared.Features.Spider.Commands;
+
+namespace SpiderControl.WebApiV2.Features.Spider;
+
+[ApiController]
+[Route("api/v2/[controller]")]
+public class SpiderController : ControllerBase
+{
+    [HttpPost("process")]
+    public ActionResult<ProcessSpiderCommandResponse> Process([FromBody] ProcessSpiderCommandRequest request)
+    {
+        return Ok(new ProcessSpiderCommandResponse { FinalPosition = string.Empty });
+    }
+}
